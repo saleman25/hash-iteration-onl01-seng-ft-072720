b01fc3cb26f3fe9f1b854e2e90d1d4fae94aca14
Samantha Aleman <samantha.aleman25@gmail.com>
2020-07-31 04:43:02 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,5 +14,5 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.collect do |kids_name, age|
-    if age < 12
-      
+    if age <= 12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
